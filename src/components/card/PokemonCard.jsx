ddfd2c7a1c813f24b14528818b73345e268639dc
Pablo Herrera <paaherre>
2023-02-06 17:18:40 -0300

    PokeCard info

--- src/components/card/PokemonCard.jsx
+++ src/components/card/PokemonCard.jsx
@@ -1,7 +1,16 @@
-const PokemonCard = ({ pokemon }) => {
+import "./PokemonCard.css"
 
+const PokemonCard = ({ pokemon }) => {
+    console.log(pokemon)
     return (
-        <div>Pokemon Card</div>
+        <div className="card m-1">
+            <img src={pokemon.sprites.front_default} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+                <h5 className="card-title">{pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</h5>
+                <p className="card-text">Tipo: {pokemon.types.map(types => types.type.name)}</p>
+                <p className="card-text">Número: {pokemon.id}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -25,7 +25,6 @@ function Home() {
         loadPokemon();
     }, [])
 
-
     return (
         <>
             <div className="home">
