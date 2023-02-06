86b41a3411f97fbd815f83fc971c981e103b0570
Pablo Herrera <paaherre>
2023-02-06 16:38:17 -0300

    Agregado buscador y funcionalidad

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,6 +5,9 @@ function Home() {
 
     const [pokemonList, setPokemonList] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [pokeFilter, setPokeFilter] = useState("");
+
+
     useEffect(() => {
         async function loadPokemon() {
             const api = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
@@ -17,13 +20,23 @@ function Home() {
             const results = await Promise.all(pokemones)
             setPokemonList(results.filter(poke => poke.is_default && poke.sprites.front_default))
             setLoading(false)
+
         }
         loadPokemon();
     }, [])
 
+
     return (
         <>
-            {loading ? <h1>Loading</h1> : pokemonList.map(pokemon => <PokemonCard pokemon={pokemon} />)}
+            <div className="home">
+                <div className="input-group mb-3 p-5">
+                    <input type="text" className="form-control" placeholder="Pokemon" aria-label="Pokemon" aria-describedby="basic-addon1" onChange={e => setPokeFilter(e.target.value.toLowerCase())}></input>
+                    <span className="input-group-text" id="basic-addon2">🔍</span>
+                </div>
+                {
+                    loading ? <h1>Loading</h1> : pokemonList.filter(poke => poke.name.includes(pokeFilter)).map(pokemon => <PokemonCard pokemon={pokemon} />)
+                }
+            </div>
         </>
     )
 }
